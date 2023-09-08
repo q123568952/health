7578196ecef30eea3d6c44f3e6b32eeae74b2582
Morgan Huang <m800313@gmail.com>
2023-09-08 15:32:36 +0800

    set try catch and return result

--- health_parent/health_backend/src/main/java/com/morgan/controller/CheckItemController.java
+++ health_parent/health_backend/src/main/java/com/morgan/controller/CheckItemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.morgan.constant.MessageConstant;
 import com.morgan.entity.Result;
 import com.morgan.pojo.CheckItem;
 import com.morgan.service.CheckItemService;
@@ -21,12 +22,13 @@ public class CheckItemController {
 
     @RequestMapping("/add")
     public Result add(@RequestBody CheckItem checkItem){
-
-        checkItemService.add(checkItem);
-
-       
-
-        return null;
+        try{
+            checkItemService.add(checkItem);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.ADD_CHECKITEM_FAIL);
+        }
+        return new Result(false, MessageConstant.ADD_CHECKITEM_SUCCESS);
 
     }
     
