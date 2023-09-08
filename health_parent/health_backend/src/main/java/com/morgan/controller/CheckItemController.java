be94f7223873efda2e4877a12c93fbaa97ffd333
Morgan Huang <m800313@gmail.com>
2023-09-08 15:27:16 +0800

    Using @reference  to find service in zookeeper

--- health_parent/health_backend/src/main/java/com/morgan/controller/CheckItemController.java
+++ health_parent/health_backend/src/main/java/com/morgan/controller/CheckItemController.java
@@ -1,21 +1,28 @@
 package com.morgan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.morgan.entity.Result;
 import com.morgan.pojo.CheckItem;
+import com.morgan.service.CheckItemService;
 
 // 檢查項管理
 @RestController
 @RequestMapping("/checkitem")
 public class CheckItemController {
 
-    
+    // 查找服務
+    @Reference
+    private CheckItemService checkItemService;
 
     @RequestMapping("/add")
     public Result add(@RequestBody CheckItem checkItem){
+
+        checkItemService.add(checkItem);
 
        
 
