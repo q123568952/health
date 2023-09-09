58daf248618c36b725fbbe70f9f2618c6df7a0c7
Morgan Huang <m800313@gmail.com>
2023-09-09 23:12:34 +0800

    create serviceimpl for checkitem

---
+++ health_parent/health_service_provider/src/main/java/com/morgan/service/impl/CheckItemServiceImpl.java
@@ -0,0 +1,23 @@
+package com.morgan.service.impl;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.morgan.pojo.CheckItem;
+import com.morgan.service.CheckItemService;
+
+// 檢查項服務
+
+@Service
+@Transactional
+public class CheckItemServiceImpl implements CheckItemService{
+
+    
+
+    @Override
+    public void add(CheckItem CheckItem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+    
+}
