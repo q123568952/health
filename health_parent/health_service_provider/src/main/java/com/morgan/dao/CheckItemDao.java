8ee829f10d64fcad956a637b4127e9b00d2744c1
Morgan Huang <m800313@gmail.com>
2023-09-11 12:47:38 +0800

    add dao

--- health_parent/health_service_provider/src/main/java/com/morgan/dao/CheckItemDao.java
+++ health_parent/health_service_provider/src/main/java/com/morgan/dao/CheckItemDao.java
@@ -1,10 +1,14 @@
 package com.morgan.dao;
 
 
+import com.github.pagehelper.Page;
+
 import com.morgan.pojo.CheckItem;
 
 public interface CheckItemDao {
 
     public void add(CheckItem CheckItem);
+
+    public Page<CheckItem> selectByCondition(String queryString);
 
 }
