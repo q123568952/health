7c5311ef0d678e5a651766cf2bfecce93fcd3b65
Morgan Huang <m800313@gmail.com>
2023-09-12 12:48:04 +0800

    create edit and findbyid method

--- health_parent/health_backend/src/main/java/com/morgan/controller/CheckItemController.java
+++ health_parent/health_backend/src/main/java/com/morgan/controller/CheckItemController.java
@@ -30,7 +30,6 @@ public class CheckItemController {
             return new Result(false, MessageConstant.ADD_CHECKITEM_FAIL);
         }
         return new Result(true, MessageConstant.ADD_CHECKITEM_SUCCESS);
-
     }
 
     @RequestMapping("/findPage")
@@ -47,7 +46,28 @@ public class CheckItemController {
             e.printStackTrace();
             return new Result(false, MessageConstant.DELETE_CHECKITEM_FAIL);
         }
-        return new Result(true, MessageConstant.DELETE_CHECKITEM_SUCCESS);
-            
+        return new Result(true, MessageConstant.DELETE_CHECKITEM_SUCCESS);  
     }
+    @RequestMapping("/findById")
+    public Result findById(Integer id){
+        try{
+            CheckItem checkItem = checkItemService.findById(id);
+            return new Result(true, MessageConstant.QUERY_CHECKITEM_SUCCESS, checkItem);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.QUERY_CHECKITEM_FAIL);
+        }
+    }
+    @RequestMapping("/edit")
+    public Result edit(@RequestBody CheckItem checkItem){
+        try{
+            checkItemService.edit(checkItem);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConstant.EDIT_CHECKITEM_FAIL);
+        }
+        return new Result(true, MessageConstant.EDIT_CHECKITEM_SUCCESS);
+
+    }
+
 }
